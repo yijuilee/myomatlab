233d98224e12d5196cf83d2e3851340c7a11c0ed
yijuilee <jackie_6196@hotmail.com>
2015-08-21 02:27:29 -0400

    Misc.

---
+++ terminate.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2014  Niklas Rosenstein
+# All rights reserved.
+
+#import sys
+#sys.exit()
+#this does not kill python scripts across different interpreter ??
+
+import os
+os.system('taskkill /f /im python.exe')
+
+
